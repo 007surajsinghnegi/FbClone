56ac1d040bac4aa9c6095f70406ac3e0c5902c96
unknown <1234surajsinghnegi@gmail.com>
2021-06-01 18:54:13 +0530

    update

---
+++ components/InputBox.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { useSession } from 'next-auth/client'
+
+function InputBox() {
+
+    const[session] = useSession();
+
+    return (
+        <div>
+               {/*Top*/}
+            <div>
+                <Image
+                    src={session.user.image}
+                    className="rounded-full"
+                    width={40}
+                    height={40}
+                    layout="fixed"
+                />
+            </div>
+
+        </div>
+    )
+}
+
+export default InputBox
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,39 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Login from '../components/Login';
+import { getSession } from 'next-auth/client'
+import Sidebar from '../components/Sidebar';
+import Feed from '../components/Feed';
 
-export default function Home() {
+export default function Home({session}) {
+
+  if(!session) return <Login/>
+
   return (
-    <div>
+    <div className="h-screen bg-gray-100 overflow-hidden">
       <Head>
         <title>Facebook Clone</title>
       </Head>
 
       <Header/>
       
-      <main>
-
+      <main className="flex">  
+          <Sidebar/>
+          <Feed/>
       </main>
       
     </div>
   )
+}
+
+export async function getServerSideProps(context){
+
+  const session = await getSession(context);
+
+  return {
+    props : {
+       session
+    }
+  }
+
 }
